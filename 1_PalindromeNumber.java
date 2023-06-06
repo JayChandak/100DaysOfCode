a7a8c4dc458237a2de3f92153d92636ea64a1764
Jay Chandak <108532357+JayChandak@users.noreply.github.com>
2023-06-06 20:31:25 +0530

    Update and rename Day1/Day_1.java to 1_PalindromeNumber.java

---
+++ 1_PalindromeNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if(x < 0)
+            return false; 
+            
+        int reversedNumber = 0, remainder;
+        int temp = x;
+
+        while(temp != 0){
+            remainder = temp % 10;
+            reversedNumber = reversedNumber *  10 + remainder;
+            temp = temp/10;
+        }
+
+        boolean res = reversedNumber == x ? true : false;
+        return res;
+    }
+}
