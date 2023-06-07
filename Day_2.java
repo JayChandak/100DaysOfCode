fb86c68256173a8f9dd0c5f516a5a51c5ec05299
Jay Chandak <108532357+JayChandak@users.noreply.github.com>
2023-06-07 20:40:26 +0530

    Update Day_2.java
    Remove Duplicates from Sorted Array

--- Day_2.java
+++ Day_2.java
@@ -1,25 +1,21 @@
 public class Day_2{
   public static int removeDuplicates(int[] nums) {
-		int res = 1;
-		int n = nums.length;
-
-		for(int i = 1;i < n;i++){
-			if(nums[i] != nums[res-1]){
-				nums[res] = nums[i];
-				res++;
-			}      
-		}
-		return res;
+	int res = 1;
+	int n = nums.length;
+	for(int i = 1;i < n;i++){
+   	    if(nums[i] != nums[res-1]){
+		nums[res] = nums[i];
+		res++;
+	    }      
 	}
-	public static void main(String[] args) {
-
-		int arr[] = {0,0,1,1,1,2,2,3,3,4};		
-		Day_2 obj = new Day_2();
-		
-		int n = removeDuplicates(arr);
-		
-		for(int i = 0; i < n;i++) {
-			System.out.print(arr[i] + " ");
-		}
+    return res;
+}
+public static void main(String[] args) {
+	int arr[] = {0,0,1,1,1,2,2,3,3,4};		
+	int n = removeDuplicates(arr);
+// Printing the modified array, till n --> {0,1,2,3,4}
+	for(int i = 0; i < n;i++) {
+		System.out.print(arr[i] + " ");
 	}
+    }
 }
